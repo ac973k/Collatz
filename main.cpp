1c28a5c75337702e249e507ef06e9544b2a12fda
ac973k <dadelin8@gmail.com>
2021-10-07 17:32:44 +0300

    fix bug

--- main.cpp
+++ main.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
 #include <sstream>
+#include <string.h>
 
 #include "collatz.hpp"
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
     if (argc <= 1) {
         std::cout << "Error! Use collatz --help" << std::endl;
     }
     else if (argc == 2) {
-        std::stringstream convert(argv[1]); // создаем переменную stringstream с именем convert, инициализируя её значением argv[1]
 
-        int myint;
-        if (!(convert >> myint)) {
-            // выполняем конвертацию
-            myint = 0; // если конвертация терпит неудачу, то присваиваем myint значение по умолчанию
+        if (strcmp(argv[1], "--help") == 0) {
+            Collatz collatz("--help");
         }
+        else {
+            std::stringstream convert(argv[1]); // создаем переменную stringstream с именем convert, инициализируя её значением argv[1]
 
-        Collatz collatz(myint);
-        collatz.getResult();
+            int myint;
+            if (!(convert >> myint)) {
+                // выполняем конвертацию
+                myint = 0; // если конвертация терпит неудачу, то присваиваем myint значение по умолчанию
+            }
+
+            Collatz collatz(myint);
+            collatz.getResult();
+        }
     }
 
     return 0;
